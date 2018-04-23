ab3df607e48a1a0937ddca8ae35e0fb3f95135f3
Irina Serova <isserova@gmail.com>
2018-04-23 11:05:25 -0700

    new exercise added

--- PlayersSort.java
+++ PlayersSort.java
@@ -15,10 +15,17 @@ class Player{
 }
 class Checker implements Comparator<Player>{
 	 
-	@Override
-	public int compare(Player o1, Player o2) {
-		return o1.score - o2.score;
-	}
+	public int compare(Player a, Player b) {
+        // If 2 Players have the same score
+        if(a.score == b.score){
+            // Order alphabetically by name
+            return a.name.compareTo(b.name);
+        }    
+
+        // Otherwise, order higher score first  
+        return ((Integer) b.score).compareTo(a.score);
+    }
+
 }
  
 

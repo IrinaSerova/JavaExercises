cf13f31030294de0ab299a6b1893f05c20601e8a
Irina Serova <isserova@gmail.com>
2018-05-14 10:36:04 -0700

    added new exersise maxRegion

---
+++ BiggestRegion.java
@@ -0,0 +1,37 @@
+package com.irinaserova;
+
+public class BiggestRegion {
+	public static int getSize(int[][] matrix, int row, int column) {
+		if (row<0 || column < 0 || row >= matrix.length || column >= matrix[row].length) {
+			return 0;
+		}
+		if (matrix[row][column] == 0) {
+			return 0;
+		}
+		matrix[row][column] = 0;
+		int size = 1;
+		for (int r = row -1; r<=row +1; r++) {
+			for (int c = column  -1; c <= column + 1; c++) {
+				if (r != row || c != column) {
+					size +=getSize(matrix, r, c);
+				}
+			}
+		}
+		return size;
+	}
+	
+	public static int getBiggestRegion (int[][] matrix) {
+		int maxRegion = 0;
+		for (int row = 0; row < matrix.length; row++) {
+			for (int column = 0; column < matrix.length; column++) {
+				if (matrix[row][column] == 1) {
+					int size = getSize(matrix, row, column);
+					maxRegion = Math.max(size,  maxRegion);
+				}
+			}
+			
+		}
+		return maxRegion;
+	}
+
+}

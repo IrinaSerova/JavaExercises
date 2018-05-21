9897c5d859bcab72a3853e38788967f255efa5ee
Irina Serova <isserova@gmail.com>
2018-05-21 10:22:29 -0700

    added new exercise

---
+++ ArrayRotate.java
@@ -0,0 +1,16 @@
+package com.irinaserova;
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+public class ArrayRotate {
+    public void rotate(int[] nums, int k) {
+        int temp, previous;
+        for (int i = 0; i < k; i++) {
+            previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
+            }
+        }
+    }
+}

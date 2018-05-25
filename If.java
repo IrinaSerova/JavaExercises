67366bcbc7ae6eff28914b280bdb23d62cc4cd15
Irina Serova <isserova@gmail.com>
2018-05-24 19:42:20 -0700

    new exercise added

--- If.java
+++ If.java
@@ -5,12 +5,7 @@ package com.irinaserova;
 // when the number reaches 4 then program ends. (use a while loop and start the number at 0)
 public class If {
 	public static void main(String[] args) {
-		//		for(int i=0;i<10;i++) {
-		//			if(i==4) {
-		//				break;
-		//			}
-		//			System.out.println(i);
-		//		}
+
 		int i = 0;
 		while(i<10) {
 			System.out.println(i++);

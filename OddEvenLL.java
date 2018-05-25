946fee5659f752ae051ec9791783ebc5ebde0064
Irina Serova <isserova@gmail.com>
2018-05-25 10:25:53 -0700

    new Exercise Odd Even Linked List

---
+++ OddEvenLL.java
@@ -0,0 +1,22 @@
+package com.irinaserova;
+
+public class OddEvenLL {
+	public ListNode oddEvenList(ListNode head) {
+		if (head == null) return null;
+		ListNode odd = head, even = head.next, evenHead = even;
+		while (even != null && even.next != null) {
+			odd.next = even.next;
+			odd = odd.next;
+			even.next = odd.next;
+			even = even.next;
+		}
+		odd.next = evenHead;
+		return head;
+	}
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+}
+

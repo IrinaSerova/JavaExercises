af28286903fbfb8ca35828a9f84e08250d0df0d9
Irina Serova <isserova@gmail.com>
2018-05-30 11:08:24 -0700

    new exerise Find Duplicate Number added

---
+++ FindDuplicateNumber.java
@@ -0,0 +1,23 @@
+package com.irinaserova;
+
+public class FindDuplicateNumber {
+
+
+	public int FindDuplicate(int[] nums) {
+		int slow = 0;
+		int fast = 0;
+
+		do{
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		} while(slow != fast);
+
+		int find = 0;
+
+		while(find != slow){
+			slow = nums[slow];
+			find = nums[find];
+		}
+		return find;
+	}
+}

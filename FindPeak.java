2cb9eb0c1d003e0af72415afbcef9d5b6a5187a2
Irina Serova <isserova@gmail.com>
2018-06-06 10:23:47 -0700

    new exercise FindPeak added

--- FindPeak.java
+++ FindPeak.java
@@ -10,7 +10,7 @@ public class FindPeak {
     }
 	public static void main (String[] args){
         int arr[] = {1, 3, 20, 4, 1, 0};
-        int n = arr.length;
+        
         System.out.println("Index of a peak point is " +
         		findPeakElement(arr));
     }

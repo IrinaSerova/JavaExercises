dd4eeeee2888a805a3185620e276573b248f0362
Irina Serova <isserova@gmail.com>
2018-06-06 10:22:32 -0700

    new exercise FindPeak added

---
+++ FindPeak.java
@@ -0,0 +1,17 @@
+package com.irinaserova;
+
+public class FindPeak {
+	public static int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return i;
+        }
+        return nums.length - 1;
+    }
+	public static void main (String[] args){
+        int arr[] = {1, 3, 20, 4, 1, 0};
+        int n = arr.length;
+        System.out.println("Index of a peak point is " +
+        		findPeakElement(arr));
+    }
+}

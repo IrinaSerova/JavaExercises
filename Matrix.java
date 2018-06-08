3b13bfa006e8ab89e9d17f73fe55cd3dee111e7e
Irina Serova <isserova@gmail.com>
2018-06-08 11:19:47 -0700

    new exercise Matrix added

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,8 @@
 package com.irinaserova;
+//Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+//
+//Integers in each row are sorted in ascending from left to right.
+//Integers in each column are sorted in ascending from top to bottom.
 
 public class Matrix {
 

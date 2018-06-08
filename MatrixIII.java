6ad89e6666e2663a9bdcf23f2ff7ba8152039066
Irina Serova <isserova@gmail.com>
2018-06-08 11:43:19 -0700

    new exercise Matrix added

--- MatrixIII.java
+++ MatrixIII.java
@@ -3,8 +3,8 @@ package com.irinaserova;
 public class MatrixIII {
 
 	public static boolean searchMatrix(int[][] matrix, int target) {
-		int i=matrix.length-1;
-		int j=0;
+		int i = matrix.length-1;
+		int j = 0;
 		while(i >= 0 && j < matrix[0].length) {
 			if(matrix[i][j] == target) {
 				System.out.print(target + " found at row "+ i + "  and column "  + j);
@@ -13,18 +13,18 @@ public class MatrixIII {
 			else if(matrix[i][j] > target) i--;
 			else j++;
 		}
-		System.out.print("n Element not found");
+		System.out.print(target + "  not found");
 		return false;
 	}
 	public static void main(String[] args) {
 		
-		int mat[][] = { {10, 20, 30, 40},
+		int matrix[][] = { {10, 20, 30, 40},
 				{15, 25, 35, 45},
 				{27, 29, 37, 48},
 				{32, 33, 39, 50} };
 		
 
-		searchMatrix(mat, 29);
+		searchMatrix(matrix, 29);
 	}
 	
 }

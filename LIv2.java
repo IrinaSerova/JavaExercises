55cb89531cf31586064453a8c81867cf8630861c
Irina Serova <isserova@gmail.com>
2018-06-11 10:46:13 -0700

    new exercise added

--- LIv2.java
+++ LIv2.java
@@ -1,11 +1,11 @@
 package com.irinaserova;
 
 public class LIv2 {
-	public int lengthOfLIS(int[] nums) {
+	public static int lengthOfLIS(int[] nums) {
         return lengthofLIS(nums, Integer.MIN_VALUE, 0);
     }
 
-    public int lengthofLIS(int[] nums, int prev, int curpos) {
+    public static int lengthofLIS(int[] nums, int prev, int curpos) {
         if (curpos == nums.length) {
             return 0;
         }
@@ -15,5 +15,11 @@ public class LIv2 {
         }
         int nottaken = lengthofLIS(nums, prev, curpos + 1);
         return Math.max(taken, nottaken);
+    }
+    public static void main(String args[]){
+        int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+//        int n = arr.length;
+        System.out.println("Length of lis is "
+                           + lengthOfLIS(arr) + "n");
     }
 }

b8614f6ecd4cd48855facabcd4b583681f7f9a32
Irina Serova <isserova@gmail.com>
2018-06-13 10:51:46 -0700

    new exercise added Container With Most Water

--- MaxWater.java
+++ MaxWater.java
@@ -6,22 +6,19 @@ package com.irinaserova;
 
 public class MaxWater {
 	public static int maxArea(int[] height) {
-		int maxarea = 0, l = 0, r = height.length - 1;
-		while (l < r) {
-			maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
-			if (height[l] < height[r])
-				l++;
+		int maxarea = 0, left = 0, right = height.length - 1;
+		while (left < right) {
+			maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * (right - left));
+			if (height[left] < height[right])
+				left++;
 			else
-				r--;
+				right--;
 		}
 		return maxarea;
 	}
 	 public static void main(String[] args) {
 	        int a[] = {1, 5, 4, 3};
-	        
-	     
-	        
-	        System.out.print(maxArea(a));
-	     
+	                     
+	        System.out.print(maxArea(a));	     
 	    }
 }

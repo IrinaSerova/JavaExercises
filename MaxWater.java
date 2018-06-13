1a2002319074b60c36d72697a9e6e3baa9823cea
Irina Serova <isserova@gmail.com>
2018-06-13 10:48:34 -0700

    new exercise added Container With Most Water

--- MaxWater.java
+++ MaxWater.java
@@ -1,7 +1,11 @@
 package com.irinaserova;
 
+//We have to maximize the Area that can be formed between 
+//the vertical lines using the shorter line as length and the distance 
+//between the lines as the width of the rectangle forming the area.
+
 public class MaxWater {
-	public int maxArea(int[] height) {
+	public static int maxArea(int[] height) {
 		int maxarea = 0, l = 0, r = height.length - 1;
 		while (l < r) {
 			maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
@@ -12,4 +16,12 @@ public class MaxWater {
 		}
 		return maxarea;
 	}
+	 public static void main(String[] args) {
+	        int a[] = {1, 5, 4, 3};
+	        
+	     
+	        
+	        System.out.print(maxArea(a));
+	     
+	    }
 }

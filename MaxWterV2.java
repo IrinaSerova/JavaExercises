8d3946dbbd15fe3c98315cd3f990c3fc32a04a18
Irina Serova <isserova@gmail.com>
2018-06-13 11:04:14 -0700

    new exercise added Container With Most Water

---
+++ MaxWterV2.java
@@ -0,0 +1,18 @@
+package com.irinaserova;
+
+public class MaxWterV2 {
+	public int maxArea(int[] height) {
+		int maxArea = 0;
+		if (height == null || height.length <2) {
+			return maxArea;
+		}
+		int left = 0;
+		int right = height.length -1;{
+			while(left < right) {
+				int area = (right - left)
+			}
+		}
+			return maxArea;
+	}
+
+}

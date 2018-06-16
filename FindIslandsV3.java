8aa28bf76956e8b7a343ad9602f437e1a997dadc
Irina Serova <isserova@gmail.com>
2018-06-16 00:03:47 -0700

    new Exersice Fin Islands

--- FindIslandsV3.java
+++ FindIslandsV3.java
@@ -5,7 +5,8 @@ public class FindIslandsV3 {
 	    if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0)    return 0;
 	    int m = grid.length, n = grid[0].length;
 	    if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1')    return 0;
-	    boolean[][] visited = new boolean[m][n];//O(1) space: directly modify the '1' to '2' to mark grid[i][j] visited
+	    boolean[][] visited = new boolean[m][n];
+	    //O(1) space: directly modify the '1' to '2' to mark grid[i][j] visited
 	    return dfs(grid, visited, m, n, i, j);
 	}
 	private int dfs(char[][] grid, boolean[][] visited, int m, int n, int i, int j) {

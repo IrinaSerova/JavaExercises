f08f84c0726f72eba1ff1fb7a233a5658750bfe3
Irina Serova <isserova@gmail.com>
2018-06-18 15:32:47 -0700

    new exercise added 99 Bottles

---
+++ Bottles.java
@@ -0,0 +1,8 @@
+package com.irinaserova;
+
+//print out the lyrics to 99 Bottles. Bottles of what? Make a variable for what is in the bottle
+//		(so it can be easily changed) and start it out as “beer” or another beverage of your choice, 
+//		if you would like. Also, extract a variable to represent the initial number of bottles.
+public class Bottles {
+
+}

b40298f68fd1f673211d5484faf66437372de6fb
Irina Serova <isserova@gmail.com>
2018-06-25 14:52:41 -0700

    new exercise added

--- QuickSortV2.java
+++ QuickSortV2.java
@@ -2,12 +2,7 @@ package com.irinaserova;
 
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class QuickSortV2 {
 

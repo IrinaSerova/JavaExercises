e14f95a74441fd6d59155fcb734aa8578376fff4
Irina Serova <isserova@gmail.com>
2018-06-25 14:53:03 -0700

    new exercise added

--- QuickSortV4.java
+++ QuickSortV4.java
@@ -2,12 +2,9 @@ package com.irinaserova;
 
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
+
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
 
 public class QuickSortV4 {
 

a9dd4dd05f40ce0ddfc99951fc4c3835b142d90a
Irina Serova <isserova@gmail.com>
2018-06-25 14:51:12 -0700

    new exercise added

--- ContactsTrie.java
+++ ContactsTrie.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 
 public class ContactsTrie {
+	private static Scanner in;
 	public static class Node{
 		public int size;
 		public HashMap<Character, Node> children = new HashMap<Character, Node>();
@@ -50,7 +51,7 @@ public class ContactsTrie {
 		}
 	}
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		in = new Scanner(System.in);
 		int n = in.nextInt();
 		tries phoneBook = new tries();
 		for(int a0 = 0; a0 < n; a0++){

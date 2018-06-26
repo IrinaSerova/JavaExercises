339aad6ba026fe5cb6462ad0fcb87dcf3c48c380
Irina Serova <isserova@gmail.com>
2018-06-26 15:29:57 -0700

    new exercise added

--- DrawRect.java
+++ DrawRect.java
@@ -7,7 +7,12 @@ import javax.swing.JFrame;
 
 class MyCanvas extends JComponent {
 
-  public void paint(Graphics g) {
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public void paint(Graphics g) {
     g.drawRect (10, 10, 200, 200);  
   }
 }
---
+++ NumberOfLetters.java
@@ -0,0 +1,11 @@
+package com.irinaserova;
+
+public class NumberOfLetters {
+	public static void main(String[] args){
+		String River = "Missisippi";
+		int New = River.length();
+		System.out.print(New);
+					
+	}
+
+}

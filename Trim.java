4a534907db02b6fb423e66e9899583ca83f5d2dc
Irina Serova <isserova@gmail.com>
2018-06-26 15:44:23 -0700

    new exercise added

--- Trim.java
+++ Trim.java
@@ -6,7 +6,8 @@ public class Trim {
 
 	public static void main(String[] args) {
 		String test = "   I love Java  ";
-		System.out.print(test.trim());
+		System.out.println(test.trim());
+		System.out.println(test.toUpperCase());
 
 	}
 

0cc6dfff6ac7e742cc88c9ebda207b92fb6b160a
Irina Serova <isserova@gmail.com>
2018-06-26 15:08:35 -0700

    new exercise added

---
+++ RechtangleDemo.java
@@ -0,0 +1,15 @@
+package com.irinaserova;
+
+import java.awt.Rectangle;
+
+public class RechtangleDemo {
+
+	public static void main(String[] args) {
+		Rectangle box = new Rectangle(5,10,60,90);
+		
+		box.translate(200, 100);
+		
+
+	}
+
+}

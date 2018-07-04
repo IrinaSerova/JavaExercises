d77c75d9146a65006506191e35fee49a1f620461
Irina Serova <isserova@gmail.com>
2018-07-04 14:00:57 -0700

    new exercise added

--- DecimalToHexadecimal.java
+++ DecimalToHexadecimal.java
@@ -12,7 +12,7 @@ public class DecimalToHexadecimal {
 		int dec_num, rem;
 		String hexdec_num="";
 
-		/* hexadecimal number digits */
+//		/* hexadecimal number digits */
 
 		char hex[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 

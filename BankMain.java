c6f99389fc08383bf08e9f3c3a2e7e5187665e77
Irina Serova <isserova@gmail.com>
2018-07-04 14:00:15 -0700

    new exercise added

--- BankMain.java
+++ BankMain.java
@@ -7,7 +7,7 @@ public class BankMain {
     public static void main(String[] args) {
         Bank myBankAccount = new Bank(4141,
                 100);
-        // 
+        
         myBankAccount.deposit(-60);
         myBankAccount.withdraw(150);
 

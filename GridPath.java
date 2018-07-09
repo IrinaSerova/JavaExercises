517a13b68f7838c9e7307f49d035f7f610bb723c
Irina Serova <isserova@gmail.com>
2018-07-09 10:38:51 -0700

    new exercise added Grid Unique Path

--- GridPath.java
+++ GridPath.java
@@ -1,12 +1,13 @@
 package com.irinaserova;
 
+// count all possible paths from top left to bottom right
+
 public class GridPath {
 	public static int uniquePaths(int a, int b) {
 		/* If either 1 row or 1 column are there then the only way to end
            is to traverse through that row or column respectively.*/
 		if(a==1 || b==1)
 			return 1;
-	
 
 		// If diagonal movements are allowed then reduce one row or one column and add to the answer.
 		else{

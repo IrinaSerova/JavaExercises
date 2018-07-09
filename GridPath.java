2efa30395529f8bd9c672da6c304d27e7ebc0ac8
Irina Serova <isserova@gmail.com>
2018-07-09 10:33:18 -0700

    new exercise added Grid Unique Path

--- GridPath.java
+++ GridPath.java
@@ -9,16 +9,19 @@ public class GridPath {
 		/*If there are more than one row and column then u need to move 
           either right or down reducing one row or one column respectively
           and adding that way in answer*/
+
+        // If diagonal movements are allowed then 
+        // the last addition is required.
 		else{
 			int ans = 0;
 			ans = uniquePaths(a-1,b)+uniquePaths(a,b-1);
 			return ans;
 		}
-		
+
 	}
 	public static void main(String args[])
-    {
-       System.out.println(uniquePaths(3, 3));
-    }
+	{
+		System.out.println(uniquePaths(3, 3));
+	}
 }
 

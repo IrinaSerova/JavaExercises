8c350042923d8b2a8eaf134e8e9cc73fd9213f6d
Irina Serova <isserova@gmail.com>
2018-07-09 10:36:56 -0700

    new exercise added Grid Unique Path

--- GridPath.java
+++ GridPath.java
@@ -6,21 +6,19 @@ public class GridPath {
            is to traverse through that row or column respectively.*/
 		if(a==1 || b==1)
 			return 1;
-		/*If there are more than one row and column then u need to move 
-          either right or down reducing one row or one column respectively
-          and adding that way in answer*/
+	
 
-        // If diagonal movements are allowed then reduce one row or one column and add to the answer.
+		// If diagonal movements are allowed then reduce one row or one column and add to the answer.
 		else{
 			int paths = 0;
-			paths = uniquePaths(a-1,b)+uniquePaths(a,b-1);
+			paths = uniquePaths(a-1, b) + uniquePaths(a, b-1);
 			return paths;
 		}
 
 	}
 	public static void main(String args[])
 	{
-		System.out.println(uniquePaths(3, 3));
+		System.out.println(uniquePaths(4, 3));
 	}
 }
 

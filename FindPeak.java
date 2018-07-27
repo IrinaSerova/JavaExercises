f23c38a1fd77b5cc496c81cf30af27999c92affe
Irina Serova <isserova@gmail.com>
2018-07-26 18:58:20 -0700

    new styles added

--- FindPeak.java
+++ FindPeak.java
@@ -19,5 +19,5 @@ public class FindPeak {
     }
 }
 //Time complexity : O(n)O(n). We traverse the numsnums array of size nn once only.
-//
+
 //Space complexity : O(1)O(1). Constant extra space is used.

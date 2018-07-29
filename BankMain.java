7e63fa92d275c3aa2ce534babb26ee64084299e0
Irina Serova <isserova@gmail.com>
2018-07-28 23:25:15 -0700

    new exercise added

--- BankMain.java
+++ BankMain.java
@@ -1,16 +1,16 @@
-//package com.irinaserova;
-//
-//public class BankMain {
-//    public static void main(String[] args) {
-//        Bank myBankAccount = new Bank(4141,
-//                100);
-//        
-//        myBankAccount.deposit(-60);
-//        myBankAccount.withdraw(150);
-//
-//        myBankAccount.deposit(100);
-//        myBankAccount.withdraw(200);
-//
-//    }
-//
-//}
+package com.irinaserova;
+
+public class BankMain {
+    public static void main(String[] args) {
+        Bank myBankAccount = new Bank(4141,
+                100);
+        
+        myBankAccount.deposit(-60);
+        myBankAccount.withdraw(150);
+
+        myBankAccount.deposit(100);
+        myBankAccount.withdraw(200);
+
+    }
+
+}

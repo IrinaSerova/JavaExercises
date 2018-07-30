1d636c53b0875360a286070511d0ec2a09a339e1
Irina Serova <isserova@gmail.com>
2018-07-30 10:59:28 -0700

    new exercise added

--- QuickSort6.java
+++ QuickSort6.java
@@ -4,13 +4,13 @@ package com.irinaserova;
 public class QuickSort6 {
 	public static int[] Array={10, 34,2, 115,7};
 	static void swap(int num1, int num2){
-		int temp=Array[num1];
-		Array[num1]=Array[num2];
-		Array[num2]=temp;
+		int temp = Array[num1];
+		Array[num1] = Array[num2];
+		Array[num2] = temp;
 	}
 	static int partition(int left, int right, int pivot){
 		int low = left-1;
-		int high=right;
+		int high = right;
 		while(true){
 			while(Array[++low]<pivot){
 				//do nothing
@@ -33,8 +33,8 @@ public class QuickSort6 {
 			return;
 		}
 		else{
-			int pivot=Array[right];
-			int partitionPoint =partition( left,  right,  pivot);
+			int pivot = Array[right];
+			int partitionPoint = partition( left,  right,  pivot);
 			quicksort(left, partitionPoint-1);
 			quicksort(partitionPoint+1, right);
 		}

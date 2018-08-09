87b7b95e50e2fd700d2fa39b171101591553a14f
Irina Serova <isserova@gmail.com>
2018-08-09 16:14:59 -0700

    added reverse StringCounter

--- StringCounter.java
+++ StringCounter.java
@@ -8,7 +8,25 @@ public class StringCounter {
 		count(test);
 	}
 	public static void count(String x){
-		
+		char[] ch = x.toCharArray();
+		int letter = 0;
+		int space = 0;
+		int num = 0;
+		int other = 0;
+		for(int i = 0; i < x.length(); i++) {
+			if(Character.isLetter(ch[i])) {
+				letter++;
+			}
+			else if(Character.isDigit(ch[i])) {
+				num ++;
+			}
+			else if(Character.isSpaceChar(ch[i])) {
+				space++;
+			}
+			else {
+				other++;
+			}
+		}
 		
 	}
 	

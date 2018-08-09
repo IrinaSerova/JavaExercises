9fbb7d7c10a36872fc0c6da0ef5b5fb309afadee
Irina Serova <isserova@gmail.com>
2018-08-09 15:59:28 -0700

    added reverse string exercise

--- ReverseString.java
+++ ReverseString.java
@@ -7,6 +7,11 @@ public class ReverseString {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Please input a string: ");
 		char[] letters = scanner.nextLine().toCharArray();
+		for (int i = letters.length - 1; i >= 0; i--) {
+			System.out.print(letters[i]);
+		}
+		System.out.print("\n");
+		
 		
 	}
 

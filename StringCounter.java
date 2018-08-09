6a697140f7237a75b6de7e3ef3337008c00571b3
Irina Serova <isserova@gmail.com>
2018-08-09 16:20:47 -0700

    added reverse StringCounter

--- StringCounter.java
+++ StringCounter.java
@@ -1,7 +1,7 @@
 package com.irinaserova;
 //Write a Java program to count the letters, 
 // spaces, numbers and other characters of an input string.
-import java.util.Scanner;
+
 public class StringCounter {
 	public static String test = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.";
 	

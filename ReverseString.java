7ded56ff17928713b6b9e40f9bcef2482104d06f
Irina Serova <isserova@gmail.com>
2018-08-09 15:56:23 -0700

    added reverse string exercise

--- ReverseString.java
+++ ReverseString.java
@@ -1,6 +1,13 @@
 package com.irinaserova;
 // Write a Java program to reverse a string.
+import java.util.Scanner;
 
 public class ReverseString {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please input a string: ");
+		char[] letters = scanner.nextLine().toCharArray();
+		
+	}
 
 }

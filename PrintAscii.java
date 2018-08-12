c5ac16de4c78c38704103ce9a6945191a1fb0b2c
Irina Serova <isserova@gmail.com>
2018-08-11 17:17:02 -0700

    new exercise ascii added

--- PrintAscii.java
+++ PrintAscii.java
@@ -1,9 +1,14 @@
 package com.irinaserova;
 
+import java.util.Scanner;
+
 public class PrintAscii {
 	public static void main(String[] String) {
-		int chr = 'Z';
+		Scanner scanner = new Scanner(System.in);
+		String t = scanner.next();
+		
 		System.out.println("The ASCII value of Z is :"+chr);
+		scanner.close();
 	}
 
 }

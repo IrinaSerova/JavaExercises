ce9cdef3f944b48b5c89a2bd2fb3904761619a30
Irina Serova <isserova@gmail.com>
2018-08-14 19:06:40 -0700

    added new exercise check odd even numbers

--- CheckEven.java
+++ CheckEven.java
@@ -1,12 +1,17 @@
 package com.irinaserova;
 import java.util.*;
 public class CheckEven {
+	
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in); 
 		System.out.print("Input the number: ");
 		int n = in.nextInt();
 		if(n % 2 == 0) {
-			System.out.print("Number is even")
+			System.out.print("Number is even");
+		}
+		else {
+			System.out.print("Number is odd");
 		}
 		
 		

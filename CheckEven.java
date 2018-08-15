50ba74034fdf3946e1a0883d8880e9699493f488
Irina Serova <isserova@gmail.com>
2018-08-14 19:02:31 -0700

    added new exercise check odd even numbers

--- CheckEven.java
+++ CheckEven.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class CheckEven {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in); 
+		System.out.print("Input the nummer: ");
+		
 		
 	}
 

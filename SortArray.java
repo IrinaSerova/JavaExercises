c9edc0b4912ca879bd7788ec7b3cd4879af4825d
Irina Serova <isserova@gmail.com>
2018-08-16 14:56:33 -0700

    added new exercise sort Array

--- SortArray.java
+++ SortArray.java
@@ -7,6 +7,14 @@ public class SortArray {
 	public static void main(String[] args) {
 		int[] arrayFirst = {1234, 354, 46, 34564, 54, 67,45, 34, 4654};
 		String[] arraySecond = {"Java", "Python", "Javascript", "PHP"};
+		System.out.println("Original numeric array : "+Arrays.toString(arrayFirst));
+	    Arrays.sort(my_array1);
+	    System.out.println("Sorted numeric array : "+Arrays.toString(arrayFirst));
+	    
+	    System.out.println("Original string array : "+Arrays.toString(arraySecond));
+	    Arrays.sort(my_array2);
+	    System.out.println("Sorted string array : "+Arrays.toString(arraySecond));
+	    
 	
 	}
 }

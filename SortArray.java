96f7e7d7ad1e302cf3e9218c63827b7265a9dd4d
Irina Serova <isserova@gmail.com>
2018-08-16 14:55:36 -0700

    added new exercise sort Array

---
+++ SortArray.java
@@ -0,0 +1,12 @@
+package com.irinaserova;
+import java.util.Arrays;
+
+
+
+public class SortArray {
+	public static void main(String[] args) {
+		int[] arrayFirst = {1234, 354, 46, 34564, 54, 67,45, 34, 4654};
+		String[] arraySecond = {"Java", "Python", "Javascript", "PHP"};
+	
+	}
+}

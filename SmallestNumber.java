e7b40ffee95a86a3256ca47ef4d1b87133902d60
Irina Serova <isserova@gmail.com>
2018-08-17 20:59:09 -0700

    new exercise added Smalest Number

--- SmallestNumber.java
+++ SmallestNumber.java
@@ -1,5 +1,13 @@
 package com.irinaserova;
 
+import java.util.Scanner;
+
 public class SmallestNumber {
+	public static void main(String[] arg) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input the first number: ");
+		double x = in.nextDouble();
+		
+	}
 
 }

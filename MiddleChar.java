dc8a41e47c3e4c7879af4cdebfba78ff4fe2b4e9
Irina Serova <isserova@gmail.com>
2018-08-21 21:04:45 -0700

    added new exercise middle char of string

--- MiddleChar.java
+++ MiddleChar.java
@@ -3,24 +3,20 @@ package com.irinaserova;
 // Write a Java method to display the middle character of a string.
 import java.util.Scanner;
 public class MiddleChar {
-  public static void main(String[] args)
-    {
+  public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input a string: ");
         String str = in.nextLine();
         System.out.print("The middle character in the string: " + middle(str)+"\n");
     }
- public static String middle(String str)
-    {
+ public static String middle(String str) {
         int position;
         int length;
-        if (str.length() % 2 == 0)
-        {
+        if (str.length() % 2 == 0) {
             position = str.length() / 2 - 1;
             length = 2;
         }
-        else
-        {
+        else {
             position = str.length() / 2;
             length = 1;
         }

17b9695d5cfbb97873197ed473390f6dc81a1c0a
Irina Serova <isserova@gmail.com>
2018-08-23 10:03:25 -0700

    added new exercise sort Array

---
+++ IndexArray.java
@@ -0,0 +1,20 @@
+package com.irinaserova;
+
+public class IndexArray {
+	public static int findIdex(int[] array, int t) {
+		if(array == null) return -1;
+		int length = array.length;
+		int index = 0; 
+		while (index < length) {
+			if (array[index] == t) return index;
+			else index =+ 1;
+			}
+		return -1;
+		}		
+	
+public static void main(String[]arg) {
+	int[] array = {3,45,6,78,6,8,45};
+	System.out.println
+
+}
+}

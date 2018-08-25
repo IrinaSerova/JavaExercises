28e4775e09e4a351d4ad30f8c289e5ece8450350
Irina Serova <isserova@gmail.com>
2018-08-24 17:16:15 -0700

    added new exercise Leap year

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+package com.irinaserova;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input a year: ");
+		int year  = in.nextInt();
+		System.out.println(Is_Leap(year));
+	}
+	public static boolean Is_Leap(int y) {
+		
+	}
+
+}

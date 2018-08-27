08c3fbe3f62c6d0bf35a06b5dc65deef3d0c44e1
Irina Serova <isserova@gmail.com>
2018-08-26 21:42:38 -0700

    added new exercise sort Array

--- SecondLargestArray.java
+++ SecondLargestArray.java
@@ -10,8 +10,8 @@ public class SecondLargestArray {
 					1458, 2458, 1254, 1472, 2365, 
 					1456, 2165, 1457, 2456
 					};
-System.out.println("Original numeric array : "+Arrays.toString(my_array));            
-Arrays.sort(my_array);
+			System.out.println("Original numeric array : "+Arrays.toString(my_array));            
+			Arrays.sort(my_array);
 int index = my_array.length-1;
 while(my_array[index]==my_array[my_array.length-1]){
 index--;

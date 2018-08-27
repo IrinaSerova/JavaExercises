df21ade19a9643f23b721598100b65fc11dac179
Irina Serova <isserova@gmail.com>
2018-08-26 21:42:21 -0700

    added new exercise sort Array

--- SecondLargestArray.java
+++ SecondLargestArray.java
@@ -1,13 +1,15 @@
 package com.irinaserova;
 
 // find the second largest element in an array
+
 import java.util.Arrays;
 public class SecondLargestArray {
-public static void main(String[] args) {
-int[] my_array = {
-            10789, 2035, 1899, 1456, 2013, 
-            1458, 2458, 1254, 1472, 2365, 
-            1456, 2165, 1457, 2456};
+	public static void main(String[] args) {
+			int[] my_array = {
+					10789, 2035, 1899, 1456, 2013, 
+					1458, 2458, 1254, 1472, 2365, 
+					1456, 2165, 1457, 2456
+					};
 System.out.println("Original numeric array : "+Arrays.toString(my_array));            
 Arrays.sort(my_array);
 int index = my_array.length-1;
